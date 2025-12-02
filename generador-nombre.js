609a2a0f3111841ca0fbddb6e14b938b519d8388
Guillermo Villar Pinto <guillermo1858@gmail.com>
2025-12-01 21:00:27 -0300

    Feat: HTML document with styles and linked with the js script, in js read name and lastname to create the username required and shows itself in the console and the HTML document

---
+++ generador-nombre.js
@@ -0,0 +1,25 @@
+// Leer datos desde el usuario
+const nombreInput= prompt("Ingresa un nombre:");
+const apellidoInput= prompt("Ingresa un apellido:");
+
+// Extraer las partes solicitadas
+const inicialNombre= nombreInput.slice(0,1).toLowerCase();
+const TresPrimerasLetrasApellido= apellidoInput.slice(0,3).toLowerCase();
+
+// Generar numero aleatorio entre 10 y 99
+const numeroAleatorio= Math.floor(Math.random() * 90) + 10;
+
+// Guardar nombre de usuario generado y nombre completo
+const nombreCompleto= `${nombreInput} ${apellidoInput}`;
+const usuarioGenerado= `${inicialNombre}${TresPrimerasLetrasApellido}${numeroAleatorio}`;
+
+// Obtener elemento ul del DOM
+const nombreCompletoElement= document.getElementById("nombre-completo");
+const usuarioGeneradoElement= document.getElementById("usuario-generado");
+
+// Mostrar resultados en el DOM
+nombreCompletoElement.innerHTML= `Nombre completo: ${nombreCompleto}`;
+usuarioGeneradoElement.innerHTML= `Nombre de usuario generado: ${usuarioGenerado}`;
+
+// Mostrar resultados en consola
+console.log(`Tu nuevo nombre de usuario es: ${usuarioGenerado}`);
